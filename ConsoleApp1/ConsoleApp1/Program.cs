d052ccbedef24a501a27c2d846e100a392123a82
ahadtaghiyev <106157931+AhadDV@users.noreply.github.com>
2023-02-27 13:11:10 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿#region method Intro Void
+//argument
+//Sum(10,20);
+
+
+
+//Sum(1, 7);
+
+// return type olmayan method;
+
+// Method Signature
+// 1.return tipi,
+// 2.methodun adi,
+// 3.parametrlerin tipi ve sayi
+//void Sum(int num1,int num2)
+//{
+//    Console.WriteLine(num1+num2);
+//}
+
+#endregion
+
+
+//int num=100;
+//int num = Sum();
+//Console.WriteLine(num);
+
+
+//int Sum(int num1,int num2)
+//{
+
+//    return num1+num2;
+//}
+//int a = 10;
+//int b = 30;
+
+//int result = Sum(a,b);
+
+//Console.WriteLine(Sum(100,90));
+
+//int[] arr = { 1, 7, 5, 3, 8,7,6 };
+
+//Sum(100, 20,3);
+
+//void Sum(int num,params int[] numbers)
+//{
+//	int result = 0;
+//	for (int i = 0; i < numbers.Length; i++)
+//	{
+//		result += numbers[i]+num;
+//	}
+//	Console.WriteLine(result);
+//}
+
+//int[] numbers = { 1, 5, 4, 7, 6 };
+
+//foreach (int item in numbers)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Sum(10,20,99);
+
+//void Sum(int num1, int num2=9,int num3=1)
+//{
+//    Console.WriteLine("Num1="+num1);
+//    Console.WriteLine("Num2="+num2);
+//}
+
+
+//int[] numbers = { 6, 3, 7, 2, 4, 7, 4, 3, 56 };
+
+//int[] result = Sorting(numbers);
+//                                              //3,6
+//foreach (int item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+//int[] Sorting(int[] numbers)
+//{
+//    for (int i = 0; i < numbers.Length; i++)
+//    {
+//        for (int j = i + 1; j < numbers.Length; j++)
+//        {
+//            if (numbers[i] < numbers[j])
+//            {
+//                int num = numbers[i];
+//                numbers[i] = numbers[j];
+//                numbers[j] = num;
+//            }
+//        }
+//    }
+//    return numbers;
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
